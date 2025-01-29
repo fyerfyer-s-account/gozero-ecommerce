50123532b8b95c4c744776274b81d2507f64e906
fyer-fyerfyerfyer <fyerfyer@outlook.com>
2025-01-29 04:35:13 +0000

    create payment rmq frame, supplement previous rmq service

--- scripts/rmq/cart_rmq.sh
+++
@@ -1,5 +0,0 @@
-# Copy definitions file to container
-docker cp orderrmq.json rabbitmq:/etc/rabbitmq/rabbitmq-definitions.json
-
-# Import definitions
-docker exec rabbitmq rabbitmqctl import_definitions /etc/rabbitmq/rabbitmq-definitions.json
